7987a891d9526f3d63a9b4714294052cab29d864
sahanthathsara1 <sahanthathsaramasakorala@gmail.com>
2024-12-20 16:10:33 +0530

    make chages with tailwind css 😍

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homePage';
 import LoginPage from './pages/loginPage';
+import SignUpPage from './pages/signUpPage';
+import AdminHomePage from './pages/adminHomePage';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* Define your routes here */}
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/*" element={<h1>404 Not Found</h1>} />
-      </Routes>
+      <div>
+        {/* Navigation Bar */}
+        {/* <nav className="p-4 bg-blue-500 text-white">
+          <ul className="flex gap-4">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/signup">Sign Up</Link></li>
+          </ul>
+        </nav> */}
+
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+
+          <Route path='/admin/*' element={<AdminHomePage />} />
+
+
+          <Route path="/*" element={<h1 className="text-center text-red-500">404 Not Found</h1>} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
